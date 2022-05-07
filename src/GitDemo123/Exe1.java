764759b8fb5b628d1b793c239694910f67bfa000
Pratibha <pratibhashikare77@gmail.com>
2022-05-07 17:16:00 +0530

    [pratibha s]:commit4

---
+++ src/GitDemo123/Exe1.java
@@ -0,0 +1,8 @@
+package GitDemo123;
+
+public class Exe1 
+{
+public static void main(String[] args) {
+	System.out.println("pratibha here");
+}
+}
---
+++ src/GitDemo123/Exe2.java
@@ -0,0 +1,9 @@
+package GitDemo123;
+
+public class Exe2 
+{
+	public static void main(String[] args) {
+		System.out.println("github this is");
+	}
+
+}
